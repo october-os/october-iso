3e0974a827d650739075f7010fc643f3e0d09148
efournierrobert <elliott1447@gmail.com>
2026-01-01 16:19:48 -0500

    Added new packages file

--- mkiso.sh
+++ mkiso.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+postinstallpath=archiso/installer-profile/airootfs/root/postinstall
+
+mkdir $postinstallpath
+cp docs/packages/base.md $postinstallpath/packages
+
 mkdir build
 sudo mkarchiso -v -r -w /tmp/archiso-temp -o build/ archiso/installer-profile/
