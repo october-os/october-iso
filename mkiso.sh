c0bf7898f741f72c8635718abc1defb839b83e2d
efournierrobert <elliott1447@gmail.com>
2026-01-09 15:08:25 -0500

    Added AUR file

--- mkiso.sh
+++ mkiso.sh
@@ -4,6 +4,7 @@ postinstallpath=archiso/installer-profile/airootfs/root/postinstall
 
 mkdir $postinstallpath
 cp docs/packages/base.md $postinstallpath/packages
+cp docs/packages/aur.md $postinstallpath/aur
 
 mkdir build
 sudo mkarchiso -v -r -w /tmp/archiso-temp -o build/ archiso/installer-profile/
