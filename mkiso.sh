c6fff768b2c08e28aafe249b3ebc8be499c8e6a9
ariannelafraise <76917463+ariannelafraise@users.noreply.github.com>
2026-01-20 20:52:28 -0500

    remove warnings in mkiso shell script

--- mkiso.sh
+++ mkiso.sh
@@ -2,9 +2,9 @@
 
 postinstallpath=archiso/installer-profile/airootfs/root/postinstall
 
-mkdir $postinstallpath
+mkdir -p $postinstallpath
 cp docs/packages/base.md $postinstallpath/packages
 cp docs/packages/aur.md $postinstallpath/aur
 
-mkdir build
+mkdir -p build
 sudo mkarchiso -v -r -w /tmp/archiso-temp -o build/ archiso/installer-profile/
